eadab25d00f36299f0fd49b2fb393eabf18cdfac
Juanjo Martínez <juanjodevs@gmail.com>
2023-11-02 15:23:38 +0100

    Removed cloudflare integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,7 @@
-import { defineConfig, passthroughImageService } from 'astro/config';
+import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
-import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  output: "server",
-  adapter: cloudflare(),
-  image: {
-    service: passthroughImageService()
-  }
+  integrations: [tailwind()]
 });
