0a33fc968c29f2086f0124ae8d07052df5e0de3f
Juan José Martínez Calderón <juanjo@Juans-MacBook-Pro.local>
2024-05-22 23:56:00 +0200

    Eslint/Prettier changes for nvim

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,17 @@
-import { defineConfig, passthroughImageService } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import vercel from '@astrojs/vercel/serverless';
+import { defineConfig, passthroughImageService } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: vercel({
     webAnalytics: {
-      enabled: true,
-    },
+      enabled: true
+    }
   }),
   integrations: [tailwind()],
   image: {
     service: passthroughImageService()
   }
-});
+})
---
+++ eslint.config.js
@@ -0,0 +1,28 @@
+import eslintPluginAstro from 'eslint-plugin-astro'
+export default [
+  // add more generic rule sets here, such as:
+  // js.configs.recommended,
+  ...eslintPluginAstro.configs.recommended,
+  {
+    rules: {
+      // override/add rules settings here, such as:
+      // "astro/no-set-html-directive": "error"
+      'quotes': [
+        'error',
+        'single'
+      ],
+      'semi': [
+        'error',
+        'never'
+      ],
+      'space-before-function-paren': [
+        'error',
+        'never'
+      ],
+      'comma-dangle': [
+        'error',
+        'never'
+      ]
+    }
+  }
+]
